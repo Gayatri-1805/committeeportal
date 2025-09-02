3af3e0b675b1950197f04d0b935076dede2542c2
Gayatri-1805 <gayatri.warty18@gmail.com>
2025-09-02 17:23:16 +0530

    Revert back to commit 8414943 state

--- src/main/java/com/example/committeeportal/CommitteeportalApplication.java
+++ src/main/java/com/example/committeeportal/CommitteeportalApplication.java
@@ -1,12 +1,13 @@
 package com.example.committeeportal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommitteePortalApplication {
+public class CommitteeportalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CommitteePortalApplication.class, args);
+		SpringApplication.run(CommitteeportalApplication.class, args);
 	}
 
 }
